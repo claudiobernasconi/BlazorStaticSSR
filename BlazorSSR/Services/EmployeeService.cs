df9f247fb0cc1febbfd338edd96e2a278a4c1650
Claudio Bernasconi <email@claudio-bernasconi.net>
2024-10-07 17:13:07 +0200

    Initial commit

---
+++ BlazorSSR/Services/EmployeeService.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorSSR.Services;
+
+public class EmployeeService : IEmployeeService
+{
+    public async Task<IList<Employee>> GetEmployees()
+    {
+        var employees = new List<Employee>
+        {
+            new Employee("Norbert Hugh", "CEO", new DateOnly(2022, 8, 1)),
+            new Employee("Sonia Balmer", "Head of HR", new DateOnly(2022, 8, 1)),
+            new Employee("Peter Jackson", "HR Assistant", new DateOnly(2023, 7, 16)),
+            new Employee("Quincy Rover", "Head of Marketing", new DateOnly(2020, 1, 1)),
+            new Employee("John Doe", "Software Developer", new DateOnly(2020, 2, 1)),
+            new Employee("Sabrina Walsh", "Software Developer", new DateOnly(2020, 1, 1))
+        };
+
+        await Task.Delay(1000);
+
+        return employees;
+    }
+}
---
+++ BlazorSSR/Services/IEmployeeService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorSSR.Services;
+
+public interface IEmployeeService
+{
+    Task<IList<Employee>> GetEmployees();
+}
+
+public record Employee(string Name, string Role, DateOnly StartingDay);
